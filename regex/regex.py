2296843711949b40003277c0673966c3cb44f0e6
Praveen Bathula <praveen.bathula@everest.engineering>
2022-12-17 15:01:30 +0530

    wraps function

--- regex/regex.py
+++ regex/regex.py
@@ -1,4 +1,5 @@
 import re
+from functools import wraps
 
 domains = '''
 https://www.google.com
@@ -10,3 +11,40 @@ https://www.nasa.gov
 domain_name_pattern = re.compile(r'https?://(www.)?(\w+)(\.\w+)')
 
 print(domain_name_pattern.sub(r'\2', domains))
+
+
+def function_name(name):
+    def outer_wrapper(f):
+        class FuncWithName:
+
+            def __call__(self, *args, **kwargs):
+                return f(*args, **kwargs)
+
+            def __str__(self):
+                return name
+
+        return wraps(f)(FuncWithName())
+
+    return outer_wrapper
+
+
+def ask_next(f):
+    @wraps(f)
+    def ask(*args, **kwargs):
+        x = input(f"Do you want to run {f}: ")
+        if x:
+            f(*args, **kwargs)
+
+    return ask
+
+
+@function_name("hello func")
+@ask_next
+def hello():
+    """prints 'Hello' to the console"""
+    print("Hello")
+
+
+print(hello)
+print(hello.__doc__)
+hello()
