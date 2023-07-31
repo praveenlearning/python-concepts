fb6550459ca182d92beb2e2be3c475bdcff73191
Praveen Bathula <praveenbathula7@gmail.com>
2023-07-31 18:45:56 +0530

    implement zip function

--- main.py
+++ main.py
@@ -9,8 +9,20 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
+def zipped(*iterables):
+    print(iterables)
+    pools = [tuple(pool) for pool in iterables]
+    length = len(pools[0])
+    for i in range(length):
+        try:
+            yield tuple(pool[i] for pool in pools)
+        except IndexError:
+            return
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print(list(zipped(range(50, 100), range(1000, 1050), range(101, 150))))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
